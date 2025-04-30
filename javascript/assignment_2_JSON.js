90faedafa004c8cd92c93f7b7d93d33e105a93db
rachelwanq <w.rachel250@gmail.com>
2025-04-29 21:31:55 -0400

    voice using annyang

---
+++ javascript/assignment_2_JSON.js
@@ -0,0 +1,32 @@
+// annyang features
+function TurnAudioOff(){
+    if (annyang){
+        annyang.abort();
+    }
+}
+
+function TurnAudioOn(){
+    if (annyang){
+        annyang.start();
+    }
+}
+
+if (annyang){
+    const commands = {
+        "hello": () => {alert("Hello World!");},
+        "change the color to *color": (color) => {document.body.style.backgroundColor = color;},
+        "navigate to *page": (page) => {
+            responsepage = page.toLowerCase();
+            if (responsepage === "home"){
+                window.location.href = "home.html";
+            }
+            else if (responsepage === "stocks"){
+                window.location.href = "stocks.html";
+            }
+            else if (responsepage === "dogs"){
+                window.location.href = "dogs.html";}
+        }
+    };
+    annyang.addCommands(commands);
+
+}
